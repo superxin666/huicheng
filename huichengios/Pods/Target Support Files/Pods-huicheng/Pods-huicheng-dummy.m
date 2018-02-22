22484ba570949a5c9c3d7eef1a4852d711818459
lvxin <18730231789@163.com>
2018-02-22 17:57:58 +0800

    1

---
+++ huichengios/Pods/Target Support Files/Pods-huicheng/Pods-huicheng-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_huicheng : NSObject
+@end
+@implementation PodsDummy_Pods_huicheng
+@end
---
+++ huichengios/huicheng/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  huicheng
+//
+//  Created by lvxin on 2018/2/22.
+//  Copyright © 2018年 lvxin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
