d014b5766640863f816a19c67af1613fbf50eeeb
lvxin <18730231789@163.com>
2018-02-23 13:32:10 +0800

    4

---
+++ huichengios/huicheng/Tool/HCViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HCViewController.swift
+//  huicheng
+//
+//  Created by lvxin on 2018/2/23.
+//  Copyright © 2018年 lvxin. All rights reserved.
+//
+
+import UIKit
+
+class HCViewController: UINavigationController {
+    override init(rootViewController: UIViewController) {
+        super.init(rootViewController: rootViewController)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
