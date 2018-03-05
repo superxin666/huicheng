2b55e50a3e0e40e8fe108e0ca9cd84f93481aff6
lvxin <18730231789@163.com>
2018-03-05 11:12:23 +0800

    net1

---
+++ huichengios/huicheng/Net/BaseNetViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BaseNetViewController.swift
+//  huicheng
+//
+//  Created by lvxin on 2018/3/5.
+//  Copyright © 2018年 lvxin. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import ObjectMapper
+import SwiftyJSON
+
+class BaseNetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
