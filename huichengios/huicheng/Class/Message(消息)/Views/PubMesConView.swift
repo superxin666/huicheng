522cff22393c394683cf44d77c9cfdfd347925a8
lvxin <18730231789@163.com>
2018-03-19 17:25:53 +0800

    公告内容

--- huichengios/huicheng/Class/Message(消息)/Views/PubMesConView.swift
+++ huichengios/huicheng/Class/Message(消息)/Views/PubMesConView.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class PubMesConView: UIView,NibLoadable {
 
+    @IBOutlet weak var subLabel: UILabel!
+    @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -18,7 +22,18 @@ class PubMesConView: UIView,NibLoadable {
     }
     */
     func setData(model:newsdetialModel) {
-        
+        if let title = model.title {
+            self.titleLabel.text = title
+        }
+        if let user = model.user ,let object = model.object{
+            self.subLabel.text = "发布人：\(user)   接收对象：\(object)"
+        }
+        if let content = model.content {
+            self.contentTextView.text = content
+        }
+        if let time = model.createtime {
+            self.timeLabel.text = time
+        }
         
     }
     
