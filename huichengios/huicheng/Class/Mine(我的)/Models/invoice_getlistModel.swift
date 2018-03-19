c09cd7099199e89954beba4a57a088d6faf86640
lvxin <18730231789@163.com>
2018-03-19 21:57:24 +0800

    发票列表数据模型

--- huichengios/huicheng/Class/Mine(我的)/Models/invoice_getlistModel.swift
+++ huichengios/huicheng/Class/Mine(我的)/Models/invoice_getlistModel.swift
@@ -5,6 +5,14 @@
 //  Created by lvxin on 2018/3/19.
 //  Copyright © 2018年 lvxin. All rights reserved.
 //  发票申请  获取列表
+//"id": 450,//ID
+//"type": "增值税普通发票",//发票类型
+//"title": "4t32t34t",//发票抬头
+//"money": 454,//发票金额
+//"sendtype": "上门自取",//送达方式
+//"addtime": "2018-02-27 16:27:43",
+//申请时间 "state": "未审核"//申请状态
+
 
 import UIKit
 import ObjectMapper
@@ -12,13 +20,11 @@ import ObjectMapper
 class invoice_getlistModel: Mappable {
     var id: Int!
     var type: String!
-    var total: String!
+    var title: String!
     var money: String!
-    var state: String!
+    var sendtype: String!
     var addtime: String!
-    var stateStr: String!
-
-    
+    var state: String!
     
     init() {}
     required init?(map: Map){
@@ -28,11 +34,11 @@ class invoice_getlistModel: Mappable {
     func mapping(map: Map) {
         id <- map["id"]
         type <- map["type"]
-        total <- map["total"]
+        title <- map["title"]
         money <- map["money"]
     
+        sendtype <- map["sendtype"]
         addtime <- map["addtime"]
-        stateStr <- map["stateStr"]
         state <- map["state"]
 
     }
