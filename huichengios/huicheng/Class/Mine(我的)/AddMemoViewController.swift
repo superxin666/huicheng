fe2750d2b05ddb696c7f70bf87a5db701e7d5e5a
lvxin <18730231789@163.com>
2018-03-26 23:36:24 +0800

    添加备忘录UI

--- huichengios/huicheng/Class/Mine(我的)/AddMemoViewController.swift
+++ huichengios/huicheng/Class/Mine(我的)/AddMemoViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class AddMemoViewController: BaseViewController {
-
+    var backView : AddMemoView!
+    
+    // MARK: - life
+    override func viewWillLayoutSubviews() {
+        
+        backView.snp.makeConstraints { (make) in
+            make.top.equalTo(self.view).offset(0)
+            make.left.right.equalTo(self.view).offset(0)
+            make.height.equalTo(270)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.view.backgroundColor = viewBackColor
+        self.navigation_title_fontsize(name: "添加备忘录", fontsize: 18)
+        self.navigationBar_leftBtn_image(image: #imageLiteral(resourceName: "pub_arrow"))
+        self.navigationBar_rightBtn_title(name: "确定")
+        backView = AddMemoView.loadNib()
+        self.view.addSubview(backView)
+
+    }
+    
+    override func navigationLeftBtnClick() {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    override func navigationRightBtnClick() {
+        HCLog(message: "确定")
     }
 
     override func didReceiveMemoryWarning() {
---
+++ huichengios/huicheng/Class/Mine(我的)/Views/AddMemoView.swift
@@ -0,0 +1,21 @@
+//
+//  AddMemoView.swift
+//  huicheng
+//
+//  Created by lvxin on 2018/3/26.
+//  Copyright © 2018年 lvxin. All rights reserved.
+//
+
+import UIKit
+
+class AddMemoView: UIView, NibLoadable {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
