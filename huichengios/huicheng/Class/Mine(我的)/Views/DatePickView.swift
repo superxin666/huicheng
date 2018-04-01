47b21a85bd1a891eda76aef37fa388ef7910b539
lvxin <18730231789@163.com>
2018-04-01 22:08:48 +0800

    时间view

---
+++ huichengios/huicheng/Class/Mine(我的)/Views/DatePickView.swift
@@ -0,0 +1,28 @@
+//
+//  DatePickView.swift
+//  huicheng
+//
+//  Created by lvxin on 2018/4/1.
+//  Copyright © 2018年 lvxin. All rights reserved.
+//
+
+import UIKit
+
+class DatePickView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    @IBAction func timeChange(_ sender: UIDatePicker) {
+        
+        
+        HCLog(message: sender.date)
+        let timeStr = String.hc_getDate(date: sender.date)
+   
+    }
+    
+}
