58a965675a9f104400e6a5458422e2f65e797d88
lvxin <18730231789@163.com>
2018-08-10 17:38:58 +0800

    登录增加type

--- huichengios/huicheng/Net/LoginRequestVC.swift
+++ huichengios/huicheng/Net/LoginRequestVC.swift
@@ -28,7 +28,7 @@ class LoginRequestVC: UIViewController,BaseNetViewControllerDelegate {
     func loginRequest(u:String,p:String) {
         request.delegate = self
         let uStr : String = u.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
-        let url =   login_api + "u=\(uStr)&p=\(p)"
+        let url =   login_api + "u=\(uStr)&p=\(p)&type=IOS"
         request.request_api(url: url)
         
     }
